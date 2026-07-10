792055a09e8a85ea411e2adb1ec2a4f5ac34c72a
kidkwart <kidkwart2912@gmail.com>
2026-07-10 11:14:00 +0000

    Finalized Babel configuration and package alignment for stable SDK 54

--- babel.config.js
+++ babel.config.js
@@ -1,18 +1,23 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
     plugins: [
-      "react-native-reanimated/plugin",
+      "react-native-worklets-core/plugin",
       [
         "module-resolver",
         {
           root: ["./"],
           alias: {
             "@": "./src",
+            "react-native-worklets": "react-native-worklets-core",
           },
         },
       ],
+      "react-native-reanimated/plugin", // ALWAYS LAST
     ],
   };
 };
